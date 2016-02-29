47888bb3056fc9e575450fec4272e27bd94e17f9
darksun <lujun9972@sina.com>
2016-02-29 13:10:56 +0800

    add function `desktops-popup-restore-menu'

--- desktops.el
+++ desktops.el
@@ -1,5 +1,6 @@
 (defvar desktops-configuration-alist nil
   "")
+(defvar desktops-current-desktop-name nil)
 
 (defun desktops--read-desktop-name (&optional already-exist-desktops allow-new)
   (completing-read "desktop name: " already-exist-desktops nil (not allow-new)))
@@ -11,7 +12,8 @@
                                   (current-window-configuration))))
     (if (assoc desktop-name desktops-configuration-alist)
         (setf (cdr (assoc desktop-name desktops-configuration-alist)) window-configuration)
-      (push (cons desktop-name window-configuration) desktops-configuration-alist))))
+      (push (cons desktop-name window-configuration) desktops-configuration-alist))
+    (setq desktops-current-desktop-name desktop-name)))
 
 (defun desktops-del (&optional desktop-name)
   (interactive (list (desktops--read-desktop-name desktops-configuration-alist)))
@@ -21,7 +23,20 @@
   (interactive (list (desktops--read-desktop-name desktops-configuration-alist)))
   (let ((previous-configuration (current-window-configuration))
         (configuration (cdr (assoc desktop-name desktops-configuration-alist))))
-    (set-window-configuration configuration)
-    (desktops-save "previous" previous-configuration)))
+    (if (set-window-configuration configuration)
+        (raise-frame (window-configuration-frame configuration)))
+    (desktops-save desktops-current-desktop-name previous-configuration)
+    (desktops-save "previous" previous-configuration)
+    (setq desktops-current-desktop-name desktop-name)))
+
+(defun desktops-popup-restore-menu (&optional configuration-alist)
+  (interactive (list desktops-configuration-alist))
+  (let* ((desktop-names (mapcar #'car configuration-alist))
+         (menu-items (mapcar (lambda (name)
+                               (cons name name))
+                             desktop-names))
+         (menu `("desktop-names" ("panel" ,@menu-items)))
+         (selected-desktop-name (x-popup-menu t menu)))
+    (desktops-restore selected-desktop-name)))
 
 (provide 'desktops)
